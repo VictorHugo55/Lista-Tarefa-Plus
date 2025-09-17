a7fac53208f42f2cc217a860bf995dc2de206469
victorhugo55 <davichugopereira@gmail.com>
2025-09-17 12:02:46 -0300

    arrumadon a estilização

--- app/SettingsScreen.tsx
+++ app/SettingsScreen.tsx
@@ -1,15 +1,24 @@
 import { useTheme } from "../src/context/ThemeContext";
 import ThemeToggleButton from "../src/components/ThemeToggloButton";
 import { useTranslation } from 'react-i18next';
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 
 export default function Settings(){
     const {colors} = useTheme()
     const {t,i18n} = useTranslation()
 
     return(
-        <View style={[{backgroundColor:colors.background}]}>
+        <View style={[style.container,{backgroundColor:colors.background}]}>
             <ThemeToggleButton/>
         </View>
     )
 }
+
+const style = StyleSheet.create({
+    container: {
+    flex: 1,
+    backgroundColor: '#121212',
+    justifyContent: 'center',
+    padding: 20,
+  },
+})
